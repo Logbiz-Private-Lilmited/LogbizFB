b92a1a6aeb57449e761a526f1b329f27a4cd05b0
Sanket Kumawat <74851736+Snketkumawat@users.noreply.github.com>
2024-05-26 22:58:48 +0530

    Services And SignUP
    Co-authored-by: shiva <shivamusku8055@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { Navbar } from './components/Navbar/Navbar'
-import { HomeView } from './views/HomeView'
-import { ServicesView } from './views/ServicesView'
-import { DashboardView } from './views/DashboardView'
-import { ServicesListingView } from './views/ServicesListingView'
-import { ContactView } from './views/ContactView'
-import { LoginView } from './views/LoginView'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { HomeView } from "./views/HomeView";
+import { ServicesView } from "./views/ServicesView";
+import { DashboardView } from "./views/DashboardView";
+import { ServicesListingView } from "./views/ServicesListingView";
+import { ContactView } from "./views/ContactView";
+import { LoginView } from "./views/LoginView";
 
 function App() {
-
+  const newLocal = (
+    <div className="mt-28">
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<HomeView />} />
+        <Route path="/services" element={<ServicesView />} />
+        <Route path="/dashboard" element={<DashboardView />} />
+        <Route path="/services-listing" element={<ServicesListingView />} />
+        <Route path="/contact" element={<ContactView />} />
+        <Route path="/login" element={<LoginView />} />
+      </Routes>
+    </div>
+  );
   return (
-    <div className='flex flex-col items-center mt-4'>
-
+    <div className="flex flex-col items-center mt-4">
       <Navbar />
 
-      <div className='mt-28 w-[98%]'>
-        <Routes>
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/" element={<HomeView />} />
-          <Route path="/services" element={<ServicesView />} />
-          <Route path="/dashboard" element={<DashboardView />} />
-          <Route path="/services-listing" element={<ServicesListingView />} />
-          <Route path="/contact" element={<ContactView />} />
-          <Route path="/login" element={<LoginView />} />
-        </Routes>
-      </div>
-
+      {newLocal}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
