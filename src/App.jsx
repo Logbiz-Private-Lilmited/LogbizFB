e9dedd8855bebcc708041c3c7818cb4d2d403c03
SHIVARAM <92591279+shivaram8055@users.noreply.github.com>
2024-06-18 08:56:46 +0530

    WarehouseForm ->pushed (#7)
    Co-authored-by: Sanket Kumawat <74851736+Snketkumawat@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,25 @@ import { DashboardView } from "./views/DashboardView";
 import { ServicesListingView } from "./views/ServicesListingView";
 import { ContactView } from "./views/ContactView";
 import { LoginView } from "./views/LoginView";
+import WarehouseListing from "./components/Forms/WarehouseListing";
 
 
 
 function App() {
-  // const newLocal = (
-  //   <div className="mt-28">
-  //     <Routes>
-  //       <Route path="*" element={<Navigate to="/" />} />
-  //       <Route path="/" element={<HomeView />} />
-  //       <Route path="/services" element={<ServicesView />} />
-  //       <Route path="/dashboard" element={<DashboardView />} />
-  //       <Route path="/services-listing" element={<ServicesListingView />} />
-  //       <Route path="/contact" element={<ContactView />} />
-  //       <Route path="/login" element={<LoginView />} />
-  //     </Routes>
-  //   </div>
-  // );
+  const newLocal = (
+    <div className="mt-28">
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<HomeView />} />
+        <Route path="/services" element={<ServicesView />} />
+        <Route path="/dashboard" element={<DashboardView />} />
+        <Route path="/services-listing" element={<ServicesListingView />} />
+        <Route path="/contact" element={<ContactView />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/wform" element={<WarehouseListing />} />
+      </Routes>
+    </div>
+  );
   return (
     <div className="flex flex-col items-center mt-4">
       <Navbar />
