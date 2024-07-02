5c7974fbd39f6cb21f77379772f57e5672d19540
shiva <shivamusku8055@gmail.com>
2024-07-02 23:00:34 +0530

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,17 @@ import { DashboardView } from "./views/DashboardView";
 import { ServicesListingView } from "./views/ServicesListingView";
 import { ContactView } from "./views/ContactView";
 import { LoginView } from "./views/LoginView";
-import WarehouseListing from "./components/Forms/WarehouseListing";
-
-
+import WarehouseListing from "./components/Forms/WarehouseListingForm";
+import FSPRegistration from "./components/Forms/Registrations/FSPRegistration";
+import DistributionHubForm from "./components/Forms/Registrations/DistributionHubForm";
+import WarehouseRegistration from "./components/Forms/Registrations/WarehouseRegistration";
+import Transport from "./components/Forms/Registrations/Transport";
 
 function App() {
-  const newLocal = (
-    <div className="mt-28">
-      <Routes>
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/" element={<HomeView />} />
-        <Route path="/services" element={<ServicesView />} />
-        <Route path="/dashboard" element={<DashboardView />} />
-        <Route path="/services-listing" element={<ServicesListingView />} />
-        <Route path="/contact" element={<ContactView />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/wform" element={<WarehouseListing />} />
-      </Routes>
-    </div>
-  );
   return (
     <div className="flex flex-col items-center mt-4">
       <Navbar />
-      <div className='mt-28 w-[98%]'>
+      <div className=" mt-24 w-[98%]">
         <Routes>
           <Route path="*" element={<Navigate to="/" />} />
           <Route path="/" element={<HomeView />} />
@@ -37,6 +25,12 @@ function App() {
           <Route path="/services-listing" element={<ServicesListingView />} />
           <Route path="/contact" element={<ContactView />} />
           <Route path="/login" element={<LoginView />} />
+          <Route path="/formWarehouse" element={<WarehouseListing />} />
+          {/* <Route path="/dynamicFormTest" element={<TestForm />} /> */}
+          <Route path="formFSP" element={<FSPRegistration />} />
+          <Route path="formDistribution" element={<DistributionHubForm />} />
+          <Route path="registerWarehouse" element={<WarehouseRegistration />} />
+          <Route path="transport" element={<Transport />} />
         </Routes>
       </div>
     </div>
