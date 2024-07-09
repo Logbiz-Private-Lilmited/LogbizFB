3d43dded50f58eb6463ec35e828e79c6f17d1844
Solomon216 <solomonparthi3@gmail.com>
2024-07-09 12:08:10 +0530

    Updates

--- src/components/ServicePages/Dropdown.jsx
+++ src/components/ServicePages/Dropdown.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from 'react';
 
-const Dropdown = ({onSelect }) => {
+const options = ["City", "Capacity", "Body Type"];
+
+const Dropdown = ({ onSelect }) => {
   const [selectedOption, setSelectedOption] = useState(options[0]);
 
   const handleSelectChange = (event) => {
     const value = event.target.value;
     setSelectedOption(value);
-    onSelect(value);
+    if (onSelect) {
+      onSelect(value);
+    }
   };
 
-  const options=["City","Capacity","Body Type"]
-
   return (
-    <div className="relative inline-block w-fit text-black font-semibold p-1">
+    <div className="relative inline-block w-fit text-black font-semibold">
       <select
         value={selectedOption}
         onChange={handleSelectChange}
+        className="border-2 border-[#E9E7E2] p-1 rounded-md"
       >
         {options.map((option, index) => (
           <option key={index} value={option}>
