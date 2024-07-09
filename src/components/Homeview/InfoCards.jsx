9327f7d8392b6786c0d5c41bfa7bdf4cfad4aee8
Sanket <sanketkumawat25@gmail.com>
2024-07-09 21:49:25 +0530

    .

--- src/components/Homeview/InfoCards.jsx
+++ src/components/Homeview/InfoCards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InfoComponent = ({ heading, paragraphs, buttonText }) => {
   return (
-    <div className="flex flex-col w-3/4 max-h-fit flex-wrap self-center sm:w-10/12">
+    <div className="flex flex-col w-3/4 max-h-fit flex-wrap mx-auto">
       <h2 className="text-4xl font-bold flex-1 leading-h2 mb-10">{heading}</h2>
 
       {paragraphs.map((paragraph, index) => (
@@ -21,7 +21,7 @@ const InfoComponent = ({ heading, paragraphs, buttonText }) => {
 const InfoCards = ({ info }) => {
   return (
     <div className="py-10">
-      <div className="flex lg:flex-row md:flex-row md:justify-center flex-col ">
+      <div className="flex lg:flex-row md:flex-row md:justify-center flex-col">
         {info.map((box, index) => (
           <div key={index}>
             <InfoComponent
