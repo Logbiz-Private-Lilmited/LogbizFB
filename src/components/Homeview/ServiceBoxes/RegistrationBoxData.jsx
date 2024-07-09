60b32fce7b0043419e59f5c3d9ec24d3332c4ba8
Sanket <sanketkumawat25@gmail.com>
2024-07-09 21:04:12 +0530

    Improved and Added box slider component

---
+++ src/components/Homeview/ServiceBoxes/RegistrationBoxData.jsx
@@ -0,0 +1,44 @@
+const RegBoxData = [
+    {
+        svgPath:'/truck.svg',
+        text: 'Road transportation Registration',
+        link: '/box1',
+      },
+      {
+        svgPath: '/international-shipping 1.svg',
+        text: 'Freight Forwarding Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/warehouse 1.svg',
+        text: 'Warehouse Service Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/distribution 1.svg',
+        text: 'Distribution Service Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/insurance 1.svg',
+        text: 'Insurance Service Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/consultant 1.svg',
+        text: 'Logistics Consulting Services Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/manufacturing 1.svg',
+        text: 'OEM Services Registration',
+        link: '/box2',
+      },
+      {
+        svgPath: '/finance 1.svg',
+        text: 'Finance Service Registration',
+        link: '/box2',
+      },
+]
+
+export default RegBoxData;
