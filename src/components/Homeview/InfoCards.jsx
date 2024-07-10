f3f0fe955748cc75c158f03dafde8b72505f4ded
Sanket <sanketkumawat25@gmail.com>
2024-07-10 20:55:49 +0530

    Added button functinoality in infocard component

--- src/components/Homeview/InfoCards.jsx
+++ src/components/Homeview/InfoCards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const InfoComponent = ({ heading, paragraphs, buttonText }) => {
+const InfoComponent = ({ heading, paragraphs, buttonText, buttonLink }) => {
   return (
     <div className="flex flex-col w-3/4 max-h-fit flex-wrap mx-auto">
       <h2 className="text-4xl font-bold flex-1 leading-h2 mb-10">{heading}</h2>
@@ -11,9 +12,11 @@ const InfoComponent = ({ heading, paragraphs, buttonText }) => {
         </p>
       ))}
 
-      <button className="ml-auto my-5 bg-btn-grey px-5 py-1 rounded-md font-bold hover:bg-black hover:text-white">
-        {buttonText}
-      </button>
+      <Link to={buttonLink} className="ml-auto my-5">
+        <button className="bg-btn-grey px-5 py-1 rounded-md font-bold hover:bg-black hover:text-white">
+          {buttonText}
+        </button>
+      </Link>
     </div>
   );
 };
@@ -28,6 +31,7 @@ const InfoCards = ({ info }) => {
               heading={box.heading}
               paragraphs={box.paragraphs}
               buttonText={box.buttonText}
+              buttonLink={box.buttonLink}
             />
             <hr className="block lg:hidden md:hidden w-11/12 my-5 border-black border-y-4 m-auto" />
           </div>
--- src/components/Homeview/InfoDetails.jsx
+++ src/components/Homeview/InfoDetails.jsx
@@ -1,37 +1,44 @@
-const infoDetails =[
-    {
-        heading:(
-            <>
-            Sign Up as an Onboard <span className="text-customOrange">Customer</span> in just <span className="sm:italic text-customOrange">3 steps</span>
-            </>
-        ),
-        paragraphs:[
-            "Register as a Logistic service provider. ",
-            "Complete KYC.",
-            "List your services and connect with customers.",
-            (
-            <span className="sm:italic"><br/>You are going to gear up your business</span>
-        ),
-        ],
-        buttonText:"Register Now"
-    },
-    {
-        heading:(
-            <>
-            Sign Up as a <span className="text-customOrange">Logistic service provider</span> in just <span className="sm:italic text-customOrange">3 steps</span>
-            </>
-        ),
-        paragraphs:[
-            "Register as a customer.",
-            "Place your service request .",
-            "Settle your transaction.",
-            (
-            
-            <span className="sm:italic"><br/>You will Unlock seamless transportation</span>
-        ),
-        ],
-        buttonText:"Register Now"
-    }
+const infoDetails = [
+  {
+    heading: (
+      <>
+        Sign Up as an Onboard{" "}
+        <span className="text-customOrange">Customer</span> in just{" "}
+        <span className="sm:italic text-customOrange">3 steps</span>
+      </>
+    ),
+    paragraphs: [
+      "Register as a Logistic service provider. ",
+      "Complete KYC.",
+      "List your services and connect with customers.",
+      <span className="sm:italic">
+        <br />
+        You are going to gear up your business
+      </span>,
+    ],
+    buttonText: "Register Now",
+    buttonLink: "/",
+  },
+  {
+    heading: (
+      <>
+        Sign Up as a{" "}
+        <span className="text-customOrange">Logistic service provider</span> in
+        just <span className="sm:italic text-customOrange">3 steps</span>
+      </>
+    ),
+    paragraphs: [
+      "Register as a customer.",
+      "Place your service request .",
+      "Settle your transaction.",
+      <span className="sm:italic">
+        <br />
+        You will Unlock seamless transportation
+      </span>,
+    ],
+    buttonText: "Register Now",
+    buttonLink: "/",
+  },
 ];
 
 export default infoDetails;
