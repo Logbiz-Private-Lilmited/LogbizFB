7da998ff7ac05857c5e16c60387687ab50ce71e1
Sanket <sanketkumawat25@gmail.com>
2024-07-10 21:42:28 +0530

    Added links to service boxes

--- src/components/Homeview/ServiceBoxes/ServiceBoxData.jsx
+++ src/components/Homeview/ServiceBoxes/ServiceBoxData.jsx
@@ -1,48 +1,45 @@
 //import { ReactComponent as TruckSvg } from '../../../public/turck.svg';
 
 const boxData = [
-    {
-      svgPath:'/truck.svg',
-      text: 'Road transportation',
-      link: '/transportpage',
-    },
-    {
-      svgPath: '/international-shipping 1.svg',
-      text: 'Freight Forwarding',
-      link: '/freightpage',
-    },
-    {
-      svgPath: '/warehouse 1.svg',
-      text: 'Warehouse Service ',
-      link: '/warehousepage',
-    },
-    {
-      svgPath: '/distribution 1.svg',
-      text: 'Distribution Service',
-      link: '/box2',
-    },
-    {
-      svgPath: '/insurance 1.svg',
-      text: 'Insurance Service',
-      link: '/box2',
-    },
-    {
-      svgPath: '/consultant 1.svg',
-      text: 'Logistics Consulting Services',
-      link: '/logisticspage',
-    },
-    {
-      svgPath: '/manufacturing 1.svg',
-      text: 'Original Equipment Manufacturers Services',
-      link: '/box2',
-    },
-    {
-      svgPath: '/finance 1.svg',
-      text: 'Finance Service',
-      link: '/box2',
-    },
-  
-    
-  ];
-  export default boxData;
-  
+  {
+    svgPath: "/truck.svg",
+    text: "Road transportation",
+    link: "/transportpage ",
+  },
+  {
+    svgPath: "/international-shipping 1.svg",
+    text: "Freight Forwarding",
+    link: "/freightpage",
+  },
+  {
+    svgPath: "/warehouse 1.svg",
+    text: "Warehouse Service ",
+    link: "/warehousepage",
+  },
+  {
+    svgPath: "/distribution 1.svg",
+    text: "Distribution Service",
+    link: "/box2",
+  },
+  {
+    svgPath: "/insurance 1.svg",
+    text: "Insurance Service",
+    link: "/box2",
+  },
+  {
+    svgPath: "/consultant 1.svg",
+    text: "Logistics Consulting Services",
+    link: "/logisticspage",
+  },
+  {
+    svgPath: "/manufacturing 1.svg",
+    text: "Original Equipment Manufacturers Services",
+    link: "/box2",
+  },
+  {
+    svgPath: "/finance 1.svg",
+    text: "Finance Service",
+    link: "/box2",
+  },
+];
+export default boxData;
